71982df116d4948d2eeac727b8b309eedb2b0af9
Ben L <lentschigben@gmail.com>
2026-09-15 21:03:42 +0200

    Update async_en_decode.v

--- Lab1/async_en_decode.v
+++ Lab1/async_en_decode.v
@@ -41,13 +41,13 @@ module async_en_decode(
             else begin
             //grey
             case(gray_rot[3:0])
-                4'b1001 : led = 4'b0001;
-                4'b1101 : led = 4'b0011;
-                4'b1100 : led = 4'b0010;
+                4'b1001 : led = 4'b1000;
+                4'b1101 : led = 4'b1100;
+                4'b1100 : led = 4'b0100;
                 4'b1110 : led = 4'b0110;
-                4'b0110 : led = 4'b0100;
-                4'b0111 : led = 4'b1100;
-                4'b1111 : led = 4'b1000;
+                4'b0110 : led = 4'0010;
+                4'b0111 : led = 4'0011;
+                4'b1111 : led = 4'b0001;
                 4'b1011 : led = 4'b1001;
                 default: begin end
             endcase
