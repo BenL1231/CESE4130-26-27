9c073552a553e297b4e60b9554e1f12d67ffd25b
Ben L <lentschigben@gmail.com>
2026-09-15 21:13:07 +0200

    Update async_en_decode.v

--- Lab1/async_en_decode.v
+++ Lab1/async_en_decode.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 11.09.2024 15:11:11
-// Design Name:
-// Module Name: async_en_decode
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module async_en_decode(
