1b2c340a680c9cf3462aeb2cb14237c164e7dd2a
Ben L <lentschigben@gmail.com>
2026-09-15 21:14:24 +0200

    Update encoder_testbench.v

--- Lab1/encoder_testbench.v
+++ Lab1/encoder_testbench.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/15/2026 02:52:15 PM
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module testbench;
